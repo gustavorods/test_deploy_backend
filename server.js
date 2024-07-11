2888543cbe15c77f0133170dc5e2211aa5482e39
gustavorods <gustavo.rlsilva07@gmail.com>
2024-07-11 18:59:30 -0300

    init

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000; 
+
+app.use(express.json());
+
+app.use(express.static('public'));
+
+// Get data
+app.get('/', (req, res) => {
+    res.json({message: 'Hello world!'})
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
